ed635ad116dede65508640af626c0da44d31a191
Tilakraj <tsr.6261@gmail.com>
2026-07-02 11:26:21 +0530

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { SearchPage } from "@/pages/SearchPage";
 import { ProfileDetailPage } from "@/pages/ProfileDetailPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/profile/:username" element={<ProfileDetailPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
