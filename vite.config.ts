40fe6d01a30dcfa881bd121e1e022a4be12d7a3f
Tilakraj <tsr.6261@gmail.com>
2026-07-02 11:20:16 +0530

    Configure gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/wobb-vibe-coder-assignment/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
